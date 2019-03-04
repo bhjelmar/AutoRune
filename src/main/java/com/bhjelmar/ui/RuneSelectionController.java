f4f4256ce81319bf6fffa1cb6c37a203c488f564
bhjelmar <bhjelmar@gmail.com>
2019-03-04 15:05:50 -0500

    added donate and contribute links

--- src/main/java/com/bhjelmar/ui/RuneSelectionController.java
+++ src/main/java/com/bhjelmar/ui/RuneSelectionController.java
@@ -3,8 +3,11 @@ package com.bhjelmar.ui;
 import com.bhjelmar.Main;
 import com.bhjelmar.data.RuneSelection;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -17,6 +20,10 @@ import javafx.util.Pair;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
@@ -31,6 +38,8 @@ public class RuneSelectionController {
 	public ImageView championImage;
 	public GridPane footer;
 	public GridPane header;
+	public Hyperlink donate;
+	public Hyperlink contribute;
 
 	@FXML
 	private TabPane roleSelection;
@@ -55,6 +64,21 @@ public class RuneSelectionController {
 		roleSelection.getSelectionModel().selectedItemProperty().addListener(
 			(ov, t, t1) -> createRunesList(t1.getText())
 		);
+
+		donate.setOnAction(event -> {
+			try {
+				Desktop.getDesktop().browse(new URI("https://www.paypal.me/bhjelmar"));
+			} catch (IOException | URISyntaxException e1) {
+				e1.printStackTrace();
+			}
+		});
+		contribute.setOnAction(event -> {
+			try {
+				Desktop.getDesktop().browse(new URI("https://github.com/bhjelmar/autorune"));
+			} catch (IOException | URISyntaxException e1) {
+				e1.printStackTrace();
+			}
+		});
 	}
 
 	private void createRunesList(String role) {
